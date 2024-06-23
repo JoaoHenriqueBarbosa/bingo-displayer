3485ce17fa886b2a87e5df28a911ec97792bc0a9
joaohenriquebarbosa21 <joaohenriquebarbosa21@gmail.com>
2024-06-23 02:11:40 +0000

    last touch on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
@@ -28,67 +27,25 @@ export default function Login({
     return redirect("/");
   };
 
-  const signUp = async (formData: FormData) => {
-    "use server";
-
-    const origin = headers().get("origin");
-    const email = formData.get("email") as string;
-    const password = formData.get("password") as string;
-    const supabase = createClient();
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-      options: {
-        emailRedirectTo: `${origin}/auth/callback`,
-      },
-    });
-
-    if (error) {
-      return redirect("/login?message=Could not authenticate user");
-    }
-
-    return redirect("/login?message=Check email to continue sign in process");
-  };
-
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
+    <div className="w-full flex justify-center">
+      <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
 
       <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
         <label className="text-md" htmlFor="email">
           Email
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit it_had_border_here mb-6"
+          className="rounded-md px-4 py-2 bg-inherit border"
           name="email"
           placeholder="you@example.com"
           required
         />
-        <label className="text-md" htmlFor="password">
-          Password
+        <label className="text-md mt-0" htmlFor="password">
+          Senha
         </label>
         <input
-          className="rounded-md px-4 py-2 bg-inherit it_had_border_here mb-6"
+          className="rounded-md px-4 py-2 bg-inherit border !mb-6"
           type="password"
           name="password"
           placeholder="••••••••"
@@ -96,17 +53,10 @@ export default function Login({
         />
         <SubmitButton
           formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
+          className="bg-green-700 rounded-md px-4 py-2 text-foreground"
+          pendingText="Entrando..."
         >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="it_had_border_here it_had_border_here-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
+          Entrar
         </SubmitButton>
         {searchParams?.message && (
           <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
@@ -114,6 +64,7 @@ export default function Login({
           </p>
         )}
       </form>
+    </div>
     </div>
   );
 }
